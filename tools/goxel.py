157089230a968a428ced68708a4c72917b41f1aa
Miguel de Dios Matias <tres.14159@gmail.com>
2019-07-09 13:13:28 +0200

    Added the 'goxel parse library in python' from kaitai struct compiler and the last library for kaitaistruct.py

---
+++ tools/goxel.py
@@ -0,0 +1,243 @@
+# This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+from pkg_resources import parse_version
+from kaitaistruct import __version__ as ks_version, KaitaiStruct, KaitaiStream, BytesIO
+from enum import Enum
+
+
+if parse_version(ks_version) < parse_version('0.7'):
+    raise Exception("Incompatible Kaitai Struct Python API: 0.7 or later is required, but you have %s" % (ks_version))
+
+class Goxel(KaitaiStruct):
+    """Goxel is a 3D editor for voxel scenes.
+    
+    .. seealso::
+       Source - https://github.com/guillaumechereau/goxel/blob/master/src/formats/gox.c
+    """
+
+    class ColorTypes(Enum):
+        greyscale = 0
+        truecolor = 2
+        indexed = 3
+        greyscale_alpha = 4
+        truecolor_alpha = 6
+
+    class CompressionMethods(Enum):
+        zlib = 0
+    def __init__(self, _io, _parent=None, _root=None):
+        self._io = _io
+        self._parent = _parent
+        self._root = _root if _root else self
+        self._read()
+
+    def _read(self):
+        self.header = self._root.Header(self._io, self, self._root)
+        self.chunk = []
+        i = 0
+        while not self._io.is_eof():
+            self.chunk.append(self._root.Chunk(self._io, self, self._root))
+            i += 1
+
+
+    class Header(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.magic = self._io.ensure_fixed_contents(b"\x47\x4F\x58\x20")
+            self.version = self._io.read_u4le()
+
+
+    class Chunk(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.chunk_type = (self._io.read_bytes(4)).decode(u"ASCII")
+            self.data_size = self._io.read_u4le()
+            _on = self.chunk_type
+            if _on == u"BL16":
+                self._raw_data = self._io.read_bytes(self.data_size)
+                _io__raw_data = KaitaiStream(BytesIO(self._raw_data))
+                self.data = self._root.Chunk.Bl16(_io__raw_data, self, self._root)
+            elif _on == u"MATE":
+                self._raw_data = self._io.read_bytes(self.data_size)
+                _io__raw_data = KaitaiStream(BytesIO(self._raw_data))
+                self.data = self._root.Chunk.Mate(_io__raw_data, self, self._root)
+            elif _on == u"CAMR":
+                self._raw_data = self._io.read_bytes(self.data_size)
+                _io__raw_data = KaitaiStream(BytesIO(self._raw_data))
+                self.data = self._root.Chunk.Camera(_io__raw_data, self, self._root)
+            elif _on == u"LAYR":
+                self._raw_data = self._io.read_bytes(self.data_size)
+                _io__raw_data = KaitaiStream(BytesIO(self._raw_data))
+                self.data = self._root.Chunk.Layer(_io__raw_data, self, self._root)
+            elif _on == u"IMG ":
+                self._raw_data = self._io.read_bytes(self.data_size)
+                _io__raw_data = KaitaiStream(BytesIO(self._raw_data))
+                self.data = self._root.Chunk.Img(_io__raw_data, self, self._root)
+            else:
+                self.data = self._io.read_bytes(self.data_size)
+            self.crc = self._io.read_u4le()
+
+        class Img(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self._raw_data_img = self._io.read_bytes(self._parent.data_size)
+                _io__raw_data_img = KaitaiStream(BytesIO(self._raw_data_img))
+                self.data_img = self._root.Chunk.Dict(_io__raw_data_img, self, self._root)
+
+
+        class Camera(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self.camera_dict = []
+                i = 0
+                while not self._io.is_eof():
+                    self.camera_dict.append(self._root.Chunk.Dict(self._io, self, self._root))
+                    i += 1
+
+
+
+        class Bl16(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self.magic = self._io.ensure_fixed_contents(b"\x89\x50\x4E\x47\x0D\x0A\x1A\x0A")
+                self.png = []
+                i = 0
+                while not self._io.is_eof():
+                    self.png.append(self._root.Chunk.Bl16.ChunkPng(self._io, self, self._root))
+                    i += 1
+
+
+            class ChunkPng(KaitaiStruct):
+                def __init__(self, _io, _parent=None, _root=None):
+                    self._io = _io
+                    self._parent = _parent
+                    self._root = _root if _root else self
+                    self._read()
+
+                def _read(self):
+                    self.chunk_png_size = self._io.read_u4be()
+                    self.chunk_png_type = (self._io.read_bytes(4)).decode(u"ASCII")
+                    _on = self.chunk_png_type
+                    if _on == u"IHDR":
+                        self._raw_chunk_png_data = self._io.read_bytes(self.chunk_png_size)
+                        _io__raw_chunk_png_data = KaitaiStream(BytesIO(self._raw_chunk_png_data))
+                        self.chunk_png_data = self._root.Chunk.Bl16.ChunkPng.Ihdr(_io__raw_chunk_png_data, self, self._root)
+                    else:
+                        self.chunk_png_data = self._io.read_bytes(self.chunk_png_size)
+                    self.chunk_png_crc = self._io.read_bytes(4)
+
+                class Ihdr(KaitaiStruct):
+                    def __init__(self, _io, _parent=None, _root=None):
+                        self._io = _io
+                        self._parent = _parent
+                        self._root = _root if _root else self
+                        self._read()
+
+                    def _read(self):
+                        self.width = self._io.read_u4be()
+                        self.height = self._io.read_u4be()
+                        self.bit_depth = self._io.read_u1()
+                        self.color_type = KaitaiStream.resolve_enum(self._root.ColorTypes, self._io.read_u1())
+                        self.compression_method = KaitaiStream.resolve_enum(self._root.CompressionMethods, self._io.read_u1())
+                        self.filter_method = self._io.read_u1()
+                        self.interlace_method = self._io.read_u1()
+
+
+
+
+        class Layer(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self.count_blocks = self._io.read_u4le()
+                self.block = [None] * (self.count_blocks)
+                for i in range(self.count_blocks):
+                    self.block[i] = self._root.Chunk.Layer.Block(self._io, self, self._root)
+
+                self.block_dict = []
+                i = 0
+                while not self._io.is_eof():
+                    self.block_dict.append(self._root.Chunk.Dict(self._io, self, self._root))
+                    i += 1
+
+
+            class Block(KaitaiStruct):
+                def __init__(self, _io, _parent=None, _root=None):
+                    self._io = _io
+                    self._parent = _parent
+                    self._root = _root if _root else self
+                    self._read()
+
+                def _read(self):
+                    self.block_index = self._io.read_s4le()
+                    self.x = self._io.read_s4le()
+                    self.y = self._io.read_s4le()
+                    self.z = self._io.read_u4le()
+                    self.zero = self._io.read_u4le()
+
+
+
+        class Mate(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self.mate_dict = []
+                i = 0
+                while not self._io.is_eof():
+                    self.mate_dict.append(self._root.Chunk.Dict(self._io, self, self._root))
+                    i += 1
+
+
+
+        class Dict(KaitaiStruct):
+            def __init__(self, _io, _parent=None, _root=None):
+                self._io = _io
+                self._parent = _parent
+                self._root = _root if _root else self
+                self._read()
+
+            def _read(self):
+                self.key_size = self._io.read_u4le()
+                self.key = (self._io.read_bytes(self.key_size)).decode(u"ASCII")
+                self.value_size = self._io.read_u4le()
+                _on = self.key
+                if _on == u"name":
+                    self.value = (self._io.read_bytes(self.value_size)).decode(u"ASCII")
+                else:
+                    self.value = self._io.read_bytes(self.value_size)
+
+
+
+
