c0aafec7eee48416c89ebacc42ca50c05945998e
Miguel de Dios <tres.14159@gmail.com>
2019-07-15 01:54:37 +0200

    Upload the empty script json_to_gox.py .

---
+++ tools/json_to_gox.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import json
+
+def main(filename):
+    pass
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    main(filename)
